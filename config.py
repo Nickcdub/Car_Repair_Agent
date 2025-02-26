39a4da177d73b85bdbaf59e50df2ab55f6d4c5e5
Nickcdub <nickcdub@gmail.com>
2025-02-25 21:22:12 -0800

    Added config file, and begun adding load_document. Due to size of manual, special preprocessing needs to occur in order to parse through the data efficiently and ensure an easy query from the vectorDB. Added data to .gitignore to prevent sending 343MBs of Honda Accord Manual text.

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+# Paths
+DATA_FOLDER = "data/"
+VECTOR_DB_PATH = "vector_store/chroma_db/"
+
+# Model Parameters
+MODEL_NAME = "mistral-7b"
+
+#Embedding Models
+EMBEDDING_MODEL = "BAAI/bge-m3"
